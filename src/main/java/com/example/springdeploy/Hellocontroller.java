e5fdd6dde5645d39bb9c74c85f57899cecc97228
duvan1026 <duvancastro1026@gmail.com>
2023-04-13 14:28:39 -0500

    update file

--- src/main/java/com/example/springdeploy/Hellocontroller.java
+++ src/main/java/com/example/springdeploy/Hellocontroller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hellocontroller {
 
-    @Value("${app.varexample}")
+    @Value("${app.message}")
     String message;
 
     @GetMapping("/hola")
