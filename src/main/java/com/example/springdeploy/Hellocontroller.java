e903f6c098bf1c71e3b70736a5d6b3de747400c6
duvan1026 <duvancastro1026@gmail.com>
2023-04-13 10:49:15 -0500

    reading environment variables

--- src/main/java/com/example/springdeploy/Hellocontroller.java
+++ src/main/java/com/example/springdeploy/Hellocontroller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hellocontroller {
 
-    @Value("${app.message}")
+    @Value("${app.varexample}")
     String message;
 
     @GetMapping("/hola")
--- src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
+++ src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.springdeploy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.sql.SQLOutput;
+
+//@SpringBootTest
 class SpringDeployApplicationTests {
 
     @Test
     void contextLoads() {
+        System.getenv().forEach(
+                (key, value) -> System.out.println(key + " " + value)
+        );
     }
 
 }
